31d21d7bd9a064af99c5f01a5b1405319f895e5c
abdulwasay000 <94928964+abdulwasay000@users.noreply.github.com>
2021-11-30 23:01:36 +0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using static mid.App;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace mid
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            InventoryList.ItemsSource = GetProduct((Application.Current as App).ConnectionString);
+        }
+        private ObservableCollection<Product> GetProduct(string connectionString)
+        {
+            const string GetProductQuery = "select * from Products;";
+
+            ObservableCollection<Product> products = new ObservableCollection<Product>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    if (conn.State == System.Data.ConnectionState.Open)
+                    {
+                        using (SqlCommand cmd = conn.CreateCommand())
+                        {
+                            cmd.CommandText = GetProductQuery;
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    Product product = new Product();
+                                    product.ProductID= reader.GetInt32(0);
+                                    product.ProductName = reader.GetString(1);
+                                    product.UnitPrice = reader.GetInt32(2);
+                                    product.QuantityPerUnit = reader.GetString(3);
+                                    Product.Add(product);
+                                }
+                            }
+                        }
+                    }
+                }
+                return products;
+            }
+            catch (Exception eSql)
+            {
+                Debug.WriteLine("Exception: " + eSql.Message);
+            }
+            return null;
+        }
+    }
+}
+
